be5fa919dbb100c86fef1b9fe0f8786f68324a1c
kbbansi <kwabenaampofo5@gmail.com>
2024-02-05 13:08:37 +0000

    light reformatting

--- components/ExpenseCard.js
+++ components/ExpenseCard.js
@@ -1,8 +1,10 @@
 import {View, Text} from 'react-native';
-import { categoryBG, colours } from "../theme";
+import {categoryBG, colours} from '../theme';
 function ExpenseCard({item}) {
   return (
-    <View style={{backgroundColor: categoryBG[item.category]}} className="flex-row justify-between items-center p-3 px-5 mb-3 rounded-2xl">
+    <View
+      style={{backgroundColor: categoryBG[item.category]}}
+      className="flex-row justify-between items-center p-3 px-5 mb-3 rounded-2xl">
       <View>
         <Text className={`${colours.heading} font-bold`}>{item.title}</Text>
         <Text className={`${colours.heading} text-xs`}>{item.category}</Text>
